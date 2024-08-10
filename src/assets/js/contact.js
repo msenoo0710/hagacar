b3c5e6aa8031aacf2ef0ee6f46686701c1fca92a
msenoo0710 <M0happy2!>
2024-08-10 13:37:22 +0900

    2024年8月10日更新

---
+++ src/assets/js/contact.js
@@ -0,0 +1,34 @@
+/* /////////////////////////////////////////////////////////
+    contact.js
+///////////////////////////////////////////////////////// */
+(function($){
+    $(function(){
+
+        // 数値キーボードを変更させるための設定
+        $('#phone').attr('type', 'tel');
+        $('#zip').attr('type', 'tel');
+
+
+        // 確認ボタンの初期状態設定
+        checkPrivacyCheckbox();
+
+        // 「同意する」チェックボックスの状態変化を監視
+        $('#privacy-1').change(function() {
+            checkPrivacyCheckbox();
+        });
+
+
+    });
+})(jQuery);
+
+// 「同意する」チェックボックスの状態に応じて確認ボタンの有効/無効を切り替える関数
+function checkPrivacyCheckbox() {
+    if ($('#privacy-1').is(':checked')) {
+      // チェックされたら確認ボタンを有効化
+      $('#submit input[type=submit]').prop('disabled', false);
+    } else {
+      // チェックされていなければ確認ボタンを無効化
+      $('#submit input[type=submit]').prop('disabled', true);
+    }
+  }
+  
